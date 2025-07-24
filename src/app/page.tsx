cfe9efc0c017d6889bb3ede8154dd03696008b74
FRANKY MBIELEU <frankymbieleu@gmail.com>
2025-07-24 16:45:11 +0100

    implement bar chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
- import Image from 'next/image';
+ import { CustomBarChart } from '@/components/bar-chart';
+import Image from 'next/image';
 
 
 export default async function App() {
   await new Promise(resolve => setTimeout(resolve, 2000)) 
   return (
  <div className="grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-4 gap-4 p-4">
-    <div className="bg-primary-foreground p-4 rounded-lg shadow-md">Test de container</div>
+    <div className="bg-primary-foreground p-4 rounded-lg shadow-md col-span-2"><CustomBarChart/></div>
     <div className="bg-primary-foreground p-4 rounded-lg shadow-md">Test de container</div>
     <div className="bg-primary-foreground p-4 rounded-lg shadow-md">Test de container</div>
     <div className="bg-primary-foreground p-4 rounded-lg shadow-md">Test de container</div>
