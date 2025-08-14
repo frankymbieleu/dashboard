b30fdcff48566ff81694286e9db3dbcdffe1caa7
FRANKY MBIELEU <frankymbieleu@gmail.com>
2025-08-14 15:59:12 +0100

    Create components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
  import { ChartAreaAxes } from '@/components/area-chart';
 import { CustomBarChart } from '@/components/bar-chart';
 import { ChartPieDonutText } from '@/components/pie-chart';
+import { TodoList } from '@/components/todo-list';
 import Image from 'next/image';
 
 
@@ -11,7 +12,7 @@ export default async function App() {
     <div className="bg-primary-foreground p-4 rounded-lg shadow-md lg:col-span-2 xl:col-span-1 2xl:col-span-2 "><CustomBarChart/></div>
     <div className="bg-primary-foreground p-4 rounded-lg shadow-md">Test de container</div>
     <div className="bg-primary-foreground p-4 rounded-lg shadow-md"><ChartPieDonutText/></div>
-    <div className="bg-primary-foreground p-4 rounded-lg shadow-md">Test de container</div>
+    <div className="bg-primary-foreground p-4 rounded-lg shadow-md"><TodoList/></div>
     <div className="bg-primary-foreground p-4 rounded-lg shadow-md lg:col-span-2 xl:col-span-1 2xl:col-span-2"><ChartAreaAxes/></div>
     <div className="bg-primary-foreground p-4 rounded-lg shadow-md">Test de container</div>
       </div>
